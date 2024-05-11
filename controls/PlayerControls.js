b42326e8996790b804beeb538c8718c7ea2d7be8
Your name <wedoulilishte@gmail.com>
2024-05-11 22:32:08 +0300

    Add movement input chaining (for combos)

--- controls/PlayerControls.js
+++ controls/PlayerControls.js
@@ -1,4 +1,4 @@
-import { players } from "../main.js";
+import { players, timeForOneFrame } from "../main.js";
 
 let keybinds = {
     playerOne: {
@@ -6,20 +6,41 @@ let keybinds = {
         moveRight: "d",
         jump: "w",
         // used to let player jump while holding down the jump button. without this the player will stop jumping if they press another key (like d)
-        isJumping: false
+        isJumping: false,
+        crouch: "s",
+        // currently does nothing as crouching function is non-existent in player class
+        isCrouching: false
         // will add attacks later
     }
 
 }
+
+// how much time the player has for inputs to be chained together,
+// needs proper testing to see how it feels. have not tested it yet (attacks are not ready)
+const timeToChainInputs = timeForOneFrame * 2;
+
+// used for combos
+// inputs are: f-forward, b-backward, d-down, u-up
+let playerOneMovementInputs = [];
+let playerOneMovementTimout;
+// inputs are: '1', '2', '3', '4' (as chars not numbers)
+let playerOneAttackInputs = [];
+let playerOneAttackTimout;
+// the inputs in these comments do not mean these are the keybinds. these are what the game understands
+
+
 
 // controls for player one
 // keydown fires when the key is continuously being pressed down
 document.addEventListener("keydown", (e) =>{
     if(e.key === keybinds.playerOne.moveLeft){
         players[0].velocity.x = -1 * players[0].moveSpeed;
+        // need to come at some point when rotation is introduced and change argument based on rotation
+        handleMovementInputChain('b');
     }
     else if(e.key === keybinds.playerOne.moveRight){
         players[0].velocity.x = 1 * players[0].moveSpeed;
+        handleMovementInputChain('f');
     }
 })
 
@@ -28,6 +49,7 @@ document.addEventListener("keyup", (e) =>{
 
     // checks velocity so that player doesn't stop moving in case 2 buttons are pressed at the same time. This allows for a more fluid and tight feel
     if(e.key === keybinds.playerOne.moveLeft && players[0].velocity.x < 0){
+        // should use a setter instead of this in order to not mess with knockbacks and things
         players[0].velocity.x = 0;
     }
     else if(e.key === keybinds.playerOne.moveRight && players[0].velocity.x > 0){
@@ -36,12 +58,20 @@ document.addEventListener("keyup", (e) =>{
     else if(e.key === keybinds.playerOne.jump){
         keybinds.playerOne.isJumping = false;
     }
+    else if(e.key === keybinds.playerOne.crouch){
+        keybinds.playerOne.isCrouching = false;
+    }
 })
 
 // keypress fires when the key is pressed down
 document.addEventListener("keypress", (e) =>{
     if(e.key === keybinds.playerOne.jump){
         keybinds.playerOne.isJumping = true;
+        handleMovementInputChain('u');
+    }
+    if(e.key === keybinds.playerOne.crouch){
+        keybinds.playerOne.isCrouching = true;
+        handleMovementInputChain('d');
     }
 })
 
@@ -51,3 +81,15 @@ setInterval(()=>{
         players[0].jump();
     }
 }, 0);
+
+
+
+function handleMovementInputChain(action){
+    if(typeof(action) != "symbol"){ throw "Invalid argument. Functions takes a char"; }
+    playerOneMovementInputs.push(action);
+    if(playerOneMovementInputs.length() > 5){ playerOneMovementInputs.splice(0, 1)};
+    clearTimeout(playerOneMovementTimout);
+    playerOneMovementTimout = setTimeout(() => {
+        playerOneMovementInputs = [];
+    }, timeToChainInputs);
+}

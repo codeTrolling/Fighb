672bc3c509184b4db3be40c443a757cdf9b7dd26
Your name <wedoulilishte@gmail.com>
2024-06-03 22:52:46 +0300

    Add animation class

---
+++ classes/Animation.js
@@ -0,0 +1,10 @@
+class Animation{
+    constructor(src, totalFrames, scale, name){
+        this.src = src;
+        this.totalFrames = totalFrames;
+        this.scale = scale;
+        this.name = name;
+    }
+}
+
+export default Animation
